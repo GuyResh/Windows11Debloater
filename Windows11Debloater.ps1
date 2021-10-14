811e6b9e5001115da690899280d665ff5260c3c9
Mystic <92502536+Mystic4491@users.noreply.github.com>
2021-10-13 21:55:24 -0800

    Debloat Win11

---
+++ Windows11Debloater.ps1
@@ -0,0 +1,82 @@
+﻿("   ")
+("   ")
+("   ")
+("   ")
+("   ")
+
+
+#Credits to Mystic4491 on Github. (DO NOT REMOVE!)
+#The purpace for this scrips is so that if you reset your pc at any time you can get this script and easly debloat any windows 11 system.
+#You may edit this to your content and needs but please do not remove the credits to me on github and instead put a line under my credits line and put your name on that line instead. Thanks!
+
+
+
+("Welcome to my debloater! This is for Windows 11 ONLY and may not work on Windows 10.
+ You may have some apps already deleted and may result in this message. 
+ (No installed package found matching input criteria.) and thats ok! it will keep debloating your system and after it says done, 
+ Your system is now debloated!")
+
+winget uninstall --id Microsoft.Edge
+winget uninstall --id Microsoft.Edge Update
+winget uninstall --id Microsoft.Edge WebView2.Runtime
+winget uninstall --id Cortana
+winget uninstall --id Microsoft.News
+winget uninstall --id MSN.Weather
+winget uninstall --id Xbox
+winget uninstall --id Get.Help
+winget uninstall --id Microsoft.Tips
+winget uninstall --id HEIF.Image.Extensions
+winget uninstall --id Microsoft.Edge
+winget uninstall --id Office
+winget uninstall --id Microsoft.Solitaire.Collection
+winget uninstall --id Microsoft.Sticky.Notes
+winget uninstall --id OneDrive
+winget uninstall --id Paint
+winget uninstall --id Microsoft.People
+winget uninstall --id Power.Automate
+winget uninstall --id Microsoft.To.Do
+winget uninstall --id VP9.Video.Extensions
+winget uninstall --id Web.Media.Extensions
+winget uninstall --id Webp.Image.Extensions
+winget uninstall --id Windows.Camera
+winget uninstall --id Feedback.Hub
+winget uninstall --id Xbox.TCUI 
+winget uninstall --id Xbox.Game.Bar.Plugin
+winget uninstall --id Xbox.Game.Bar 
+winget uninstall --id Xbox.Identity.Provider
+winget uninstall --id Xbox.Game.Speech.Window
+winget uninstall --id Your.Phone
+winget uninstall --id Microsoft.Teams
+winget uninstall --id Windows.Web.Experience.Pack
+winget uninstall --id Microsoft.OneDrive
+winget uninstall --id Mail.and.Calendar
+winget uninstall --id Microsoft.Wallet_2.4.18324.0_neutral_~_8wekyb3d8bbwe
+winget uninstall --id Microsoft.SkypeApp_15.75.140.0_neutral_~_kzf8qxf38zg5c
+winget uninstall --id Microsoft.BingNews_2021.621.204.0_neutral_~_8wekyb3d8bbwe
+winget uninstall --id 7EE7776C.LinkedInforWindows_2.0.1.0_neutral_~_w1wdnht996qgy
+winget uninstall --id Microsoft.BingWeather_4.7.20002.0_neutral_~_8wekyb3d8bbwe
+winget uninstall --id Microsoft.GamingApp_2021.427.138.0_neutral_~_8wekyb3d8bbwe
+winget uninstall --id Microsoft.GetHelp_10.2108.42428.0_neutral_~_8wekyb3d8bbwe
+winget uninstall --id Microsoft.Getstarted_10.6.42361.0_neutral_~_8wekyb3d8bbwe
+winget uninstall --id Microsoft.Microsoft3DViewer_2021.2105.4012.0_neutral_~_8wekyb3d8bbwe
+winget uninstall --id Microsoft.MicrosoftOfficeHub_18.2106.12410.0_neutral_~_8wekyb3d8bbwe
+winget uninstall --id Microsoft.MixedReality.Portal_2000.21051.1282.0_neutral_~_8wekyb3d8bbwe
+winget uninstall --id Microsoft.Office.OneNote_16001.14326.20426.0_neutral_~_8wekyb3d8bbwe
+winget uninstall --id Microsoft.ScreenSketch_2021.2109.37.0_neutral_~_8wekyb3d8bbwe
+winget uninstall --id Microsoft.WindowsCamera_2022.2110.0.0_neutral_~_8wekyb3d8bbwe
+winget uninstall --id microsoft.windowscommunicationsapps_16005.14326.20436.0_neutral_~_8wekyb3d8bbwe
+winget uninstall --id Microsoft.WindowsFeedbackHub_2021.914.204.0_neutral_~_8wekyb3d8bbwe
+winget uninstall --id Microsoft.WindowsSoundRecorder_2021.2103.28.0_neutral_~_8wekyb3d8bbwe
+winget uninstall --id Microsoft.XboxGameOverlay_1.54.4001.0_neutral_~_8wekyb3d8bbwe
+winget uninstall --id Microsoft.XboxGamingOverlay_5.721.9022.0_neutral_~_8wekyb3d8bbwe
+winget uninstall --id Microsoft.XboxSpeechToTextOverlay_1.21.13002.0_neutral_~_8wekyb3d8bbwe
+winget uninstall --id Microsoft.ZuneMusic_2019.21061.10121.0_neutral_~_8wekyb3d8bbwe
+winget uninstall --id Microsoft.ZuneVideo_2019.21061.10121.0_neutral_~_8wekyb3d8bbwe
+winget uninstall --id MicrosoftWindows.Client.WebExperience_421.20034.345.0_neutral_~_cw5n1h2txyewy
+("   ")
+("   ")
+("   ")
+("   ")
+("   ")
+("   ")
+("Done! you may close this window.")
